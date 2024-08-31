bde2daea2fb9840656a577ced4d820283aeea5bf
jeevanba273 <jeevanscience273@gmail.com>
2024-08-31 22:42:16 +0530

    Updated index.html with dynamic Infura input

--- script1.py
+++ script1.py
@@ -1,16 +1,4 @@
-# # test connection
-
-# from web3 import Web3
-
-# infura_url = "https://sepolia.infura.io/v3/dde4a14c79c34a43b17ebc32f22ce6a4"
-# web3 = Web3(Web3.HTTPProvider(infura_url))
-
-# if web3.is_connected():
-#     print("Successfully connected to Infura (Sepolia)\n")
-# else:
-#     print("\nFailed to connect to Infura (Sepolia)\n")
-
-
+# test connection
 
 import sys
 from web3 import Web3
